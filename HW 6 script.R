f00d61c845528cfc5feaf102e2dfc02eba1e8ce4
Christian Panici <cpanici@luc.edu>
2020-04-06 17:52:38 -0500

    Updated imputation to include age and footspan

--- HW 6 script.R
+++ HW 6 script.R
@@ -21,7 +21,7 @@ tab = table(missing_removed$Handed, missing_removed$Season)
 tab = tab[-1,-1]
 tab
 chiperm(tab,B = 1000)
-#1. H0: There is no significant association between handedness and favorite season.
+# H0: There is no significant association between handedness and favorite season.
 
 #HA: There is a significant association between handedness and favorite season.
 
@@ -34,22 +34,20 @@ chiperm(tab,B = 1000)
 #c
 height_arm <- data.frame(ilschool$Height_cm, ilschool$Armspan_cm)
 colnames(height_arm) = c("Height", "Armspan")
-height_arm$Height = gsub("[^0-9.-]", "", height_arm)
+age_footspan = data.frame(ilschool$Ageyears, ilschool$Footlength_cm)
+colnames(age_footspan) = c("Age", "Footspan")
 
 library(mice)
 height_arm = read.csv("C:/Users/lwsc2/Documents/College Files/2020 SPRING/STAT 351/HW 6/HW 6/heightarm.csv", header = T)
+age_footspan = read.csv("C:/Users/lwsc2/Documents/College Files/2020 SPRING/STAT 351/HW 6/HW 6/agefootspan.csv", header = T)
+
+height_arm = data.frame(age_footspan[1:2], height_arm[1:2])
+
 tempdata = mice(height_arm,maxit=50,meth='cart',seed=500)
 fit = with(tempdata, lm(Height ~ Armspan))
 summary(pool(fit))
-# Intercept: 48.5136927  Std Err: 7.21623028
-# Slope: 0.7159695  Std Err: 0.04276844
 
 tempdata2 = mice(height_arm,maxit=50,meth='rf',seed=500)
 fit = with(tempdata2, lm(Height ~ Armspan))
 summary(pool(fit))
-# Intercept: 47.4342745  Std Err: 6.77751962
-# Slope: 0.7222957  Std Err: 0.04040624
-
-
-
 
